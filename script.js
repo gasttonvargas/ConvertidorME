4c86bbeffb3574da2140f54d8e55c200aa8418e4
gasttonvargas <131313901+gasttonvargas@users.noreply.github.com>
2023-11-07 16:14:34 -0300

    Se terina de hacer la pagina

--- script.js
+++ script.js
@@ -1,119 +1,111 @@
-const monedaEl_one = document.getElementById('moneda-uno');
-const monedaEl_two = document.getElementById('moneda-dos');
-const cantidadEl_one = document.getElementById('cantidad-uno');
-const cantidadEl_two = document.getElementById('cantidad-dos');
-const cambioEl = document.getElementById('cambio');
-const tazaEl = document.getElementById('taza');
+
+document.addEventListener('DOMContentLoaded', function() {
+  const monedaUno = document.getElementById('moneda-uno');
+  const monedaDos = document.getElementById('moneda-dos');
+  const cantidadUno = document.getElementById('cantidad-uno');
+  const cantidadDos = document.getElementById('cantidad-dos');
+
+  const API_URL = 'https://api.exchangerate-api.com/v4/latest/USD';
+
+  function actualizarCambio() {
+      const moneda1 = monedaUno.value;
+      const moneda2 = monedaDos.value;
+
+      fetch(API_URL)
+          .then(response => response.json())
+          .then(data => {
+              const tipoDeCambio = data.rates[moneda2] / data.rates[moneda1];
+              cantidadDos.value = (cantidadUno.value * tipoDeCambio).toFixed(2);
+          });
+  }
+
+  monedaUno.addEventListener('change', actualizarCambio);
+  monedaDos.addEventListener('change', actualizarCambio);
+  cantidadUno.addEventListener('input', actualizarCambio);
+});
+
+//Add Listen
+document.getElementById('intercambiar').addEventListener('click', () => {
+  const monedaUnoValue = document.getElementById('moneda-uno').value;
+  const monedaDosValue = document.getElementById('moneda-dos').value;
+
+  document.getElementById('moneda-uno').value = monedaDosValue;
+  document.getElementById('moneda-dos').value = monedaUnoValue;
+
+  calculate();
+});
 
 
-
-function calculate(){
-    const moneda_one = monedaEl_one.value;
-    const moneda_two = monedaEl_two.value;
-
-    if (moneda_one !== "SELECCIONE" && moneda_two !== "SELECCIONE") {
-        fetch(`https://api.exchangerate-api.com/v4/latest/${moneda_one}`)
-        .then(res => res.json() )
-        .then(data => {
-            const taza = data.rates[moneda_two];
-            
-            cambioEl.innerText = `1 ${moneda_one} = ${taza} ${moneda_two}`;
-    
-            cantidadEl_two.value = (cantidadEl_one.value * taza).toFixed(2);
-        });
-    }
-}
-
-//Event listeners
-monedaEl_one.addEventListener('change', calculate);
-cantidadEl_one.addEventListener('input', calculate);
-monedaEl_two.addEventListener('change', calculate);
-cantidadEl_two.addEventListener('input', calculate);
-
-taza.addEventListener('click', () =>{
-    const temp = monedaEl_one.value;
-    monedaEl_one.value = monedaEl_two.value;
-    monedaEl_two.value = temp;
-    calculate();
-} );
-
-
-calculate();
-
-
-// Función para agregar cotización al DOM
-function agregarCotizacion(nombre, compra, venta) {
-    const cotizacionElement = document.createElement('div');
-    cotizacionElement.classList.add('cotizacion');
-    cotizacionElement.innerHTML = `<h2>${nombre}</h2><p>Compra: ${compra}<br>Venta: ${venta}</p>`;
-    document.getElementById('cotizaciones-dolar').appendChild(cotizacionElement);
-}
-
-// Dólares
+// Función para agregar cotización Dólares
 fetch("https://dolarapi.com/v1/dolares/oficial")
   .then(response => response.json())
   .then(data => {
-    agregarCotizacion('USD Oficial', data.compra, data.venta);
+    document.querySelector('#cotizacion-oficial .card-text').innerHTML = `Compra: ${data.compra}<br>Venta: ${data.venta}`;
   });
 
 fetch("https://dolarapi.com/v1/dolares/blue")
   .then(response => response.json())
   .then(data => {
-    agregarCotizacion('USD Blue', data.compra, data.venta);
+    document.querySelector('#cotizacion-blue .card-text').innerHTML = `Compra: ${data.compra}<br>Venta: ${data.venta}`;
   });
 
 fetch("https://dolarapi.com/v1/dolares/bolsa")
   .then(response => response.json())
   .then(data => {
-    agregarCotizacion('USD Bolsa', data.compra, data.venta);
+    document.querySelector('#cotizacion-bolsa .card-text').innerHTML = `Compra: ${data.compra}<br>Venta: ${data.venta}`;
   });
 
 fetch("https://dolarapi.com/v1/dolares/contadoconliqui")
   .then(response => response.json())
   .then(data => {
-    agregarCotizacion('USD Contado con Liqui', data.compra, data.venta);
+    document.querySelector('#cotizacion-contado .card-text').innerHTML = `Compra: ${data.compra}<br>Venta: ${data.venta}`;
   });
 
 fetch("https://dolarapi.com/v1/dolares/solidario")
   .then(response => response.json())
   .then(data => {
-    agregarCotizacion('USD Solidario', data.compra, data.venta);
+    document.querySelector('#cotizacion-solidario .card-text').innerHTML = `Compra: ${data.compra}<br>Venta: ${data.venta}`;
   });
 
 fetch("https://dolarapi.com/v1/dolares/tarjeta")
   .then(response => response.json())
   .then(data => {
-    agregarCotizacion('USD Tarjeta', data.compra, data.venta);
+    document.querySelector('#cotizacion-tarjeta .card-text').innerHTML = `Compra: ${data.compra}<br>Venta: ${data.venta}`;
   });
 
 fetch("https://dolarapi.com/v1/dolares/mayorista")
   .then(response => response.json())
   .then(data => {
-    agregarCotizacion('USD Mayorista', data.compra, data.venta);
+    document.querySelector('#cotizacion-mayorista .card-text').innerHTML = `Compra: ${data.compra}<br>Venta: ${data.venta}`;
   });
 
-// Otras monedas
+  //OTRAS COTIZACIONES
 
+  
+  // Obtener cotización de EUR
 fetch("https://dolarapi.com/v1/cotizaciones/eur")
-  .then(response => response.json())
-  .then(data => {
-    agregarCotizacion('EUR', data.compra, data.venta);
-  });
+.then(response => response.json())
+.then(data => {
+  document.querySelector('#cotizacion-eur .card-text').innerHTML = `Compra: ${data.compra}<br>Venta: ${data.venta}`;
+});
 
+// Obtener cotización de BRL
 fetch("https://dolarapi.com/v1/cotizaciones/brl")
-  .then(response => response.json())
-  .then(data => {
-    agregarCotizacion('BRL', data.compra, data.venta);
-  });
+.then(response => response.json())
+.then(data => {
+  document.querySelector('#cotizacion-brl .card-text').innerHTML = `Compra: ${data.compra}<br>Venta: ${data.venta}`;
+});
 
+// Obtener cotización de CLP
 fetch("https://dolarapi.com/v1/cotizaciones/clp")
-  .then(response => response.json())
-  .then(data => {
-    agregarCotizacion('CLP', data.compra, data.venta);
-  });
+.then(response => response.json())
+.then(data => {
+  document.querySelector('#cotizacion-clp .card-text').innerHTML = `Compra: ${data.compra}<br>Venta: ${data.venta}`;
+});
 
+// Obtener cotización de UYU
 fetch("https://dolarapi.com/v1/cotizaciones/uyu")
-  .then(response => response.json())
-  .then(data => {
-    agregarCotizacion('UYU', data.compra, data.venta);
-  });
+.then(response => response.json())
+.then(data => {
+  document.querySelector('#cotizacion-uyu .card-text').innerHTML = `Compra: ${data.compra}<br>Venta: ${data.venta}`;
+});
